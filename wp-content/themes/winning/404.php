8c59aed2a2ace80f39031363e0c06c083021a5b1
aleksandraovs <aleksandra.ovsia@gmail.com>
2023-04-25 17:45:00 +0300

    front-page-> index, 404 page

---
+++ wp-content/themes/winning/404.php
@@ -0,0 +1,11 @@
+<?php get_header() ?>
+    <section>
+        <div class="container error404" style="text-align: center;">
+            <p class="title">404</p>
+            <p class="promo__subtitle" style="margin: 50px auto;">
+                Такой страницы у нас нет.
+            </p>
+            <a style="margin: 50px auto;" href="<?php echo site_url(); ?>" class="btn">Перейти на главную</a>
+        </div>
+    </section>
+<?php get_footer() ?>
